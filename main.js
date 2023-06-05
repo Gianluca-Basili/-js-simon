7c95033c862d97a77a65083109a1670f172a19fd
Gianluca-Basili <128229910+Gianluca-Basili@users.noreply.github.com>
2023-06-05 10:15:56 +0200

    add for,if end set timeout

--- main.js
+++ main.js
@@ -1,3 +1,38 @@
-function generateRandomNumbeer(min,max){
+function generateRandomNumber(min,max){
     return Math.floor(Math.random()*(max-min+1)+min);
 }
+
+function uniqueRandomNumber(array,number){
+    let check = false
+    while(check=== false){
+        
+        if(!array.includes(number)){
+            check = true
+        }
+      
+    }
+    return true;
+}
+
+let randomnumbers= [];
+let usersnumbers= [];
+
+for(let i=0; i<5; i++){
+let number =generateRandomNumber(1,50);
+
+if(uniqueRandomNumber(randomnumbers,number)){
+    randomnumbers.push(number);
+}
+}
+console.log(randomnumbers);
+document.getElementById('random-numbers').innerHTML = randomnumbers;
+
+setTimeout(function(){
+    document.getElementById('random-numbers').innerHTML = '';
+    document.getElementById('message').innerHTML = 'inserire i numeri che ti ricordi';
+
+}, 5000);
+
+
+
+
